73abd31fad440b043504d725359d0c664f7782f2
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-09-04 17:39:23 +0000

    More subreddits

--- Poster.js
+++ Poster.js
@@ -1,7 +1,7 @@
 const { POSTER_ID, POSTER_TOKEN, POSTER_TIME } = process.env;
 const request = require('node-superfetch');
 const time = Number.parseFloat(POSTER_TIME) || 1.8e+6;
-const subreddits = ['memes', 'surrealmemes', 'MemeEconomy', 'wholesomememes', 'tumblr', 'me_irl', 'blessedimages'];
+const subreddits = require('./assets/json/subreddits');
 
 setInterval(async () => {
 	try {
