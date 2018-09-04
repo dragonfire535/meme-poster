a6f0da30857f321f03c703bac3cd2ad3fff4ad42
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-09-04 00:58:39 +0000

    Initial

---
+++ Poster.js
@@ -0,0 +1,19 @@
+const { POSTER_ID, POSTER_TOKEN, POSTER_TIME } = process.env;
+const request = require('node-superfetch');
+const time = Number.parseFloat(POSTER_TIME) || 1.8e+6;
+const subreddits = ['memes', 'surrealmemes', 'MemeEconomy', 'wholesomememes', 'tumblr', 'me_irl', 'blessedimages'];
+
+setInterval(async () => {
+	try {
+		const subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
+		const { body } = await request.get(`https://www.reddit.com/r/${subreddit}/hot.json`);
+		const posts = body.data.children.filter(post => post.data && post.data.post_hint === 'image' && post.data.url);
+		if (!posts.length) throw new Error(`No images in r/${subreddit}.`);
+		const post = posts[Math.floor(Math.random() * posts.length)];
+		await request
+			.post(`https://discordapp.com/api/webhooks/${POSTER_ID}/${POSTER_TOKEN}`)
+			.send({ content: `**r/${subreddit}** ${post.title}\n${post.url}` });
+	} catch (err) {
+		console.error('[MEME POSTER]', err);
+	}
+}, time);
---
+++ README.md
@@ -0,0 +1,14 @@
+# Meme Poster
+[![Build Status](https://travis-ci.org/dragonfire535/meme-poster.svg?branch=master)](https://travis-ci.org/dragonfire535/meme-poster)
+[![Discord Server](https://discordapp.com/api/guilds/252317073814978561/embed.png)](https://discord.gg/sbMe32W)
+[![Donate on Patreon](https://img.shields.io/badge/patreon-donate-orange.svg)](https://www.patreon.com/dragonfire535)
+[![Donate on PayPal](https://img.shields.io/badge/paypal-donate-blue.svg)](https://www.paypal.me/dragonfire535)
+
+Meme Poster is a Discord Webhook coded in JavaScript. It posts memes from a
+variety of subreddits to a webhook on an interval you define in your
+`process.env`.
+
+## Licensing
+The bot is licensed under the GPL 3.0 license. See the file `LICENSE` for more
+information. If you plan to use any part of this source code in your own bot, I
+would be grateful if you would include some form of credit somewhere.
